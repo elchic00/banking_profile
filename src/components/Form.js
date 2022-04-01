355b42ddcfcd477b3df765f36a7961b5cc451f32
Andrew Alagna <aalagna04@gmail.com>
2022-04-01 18:47:23 -0400

    feat: make form view and handle logic for event
    Make an input form using materialUI and pass state values to parent component through the event handler

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,47 +2,57 @@ import { useState, useEffect } from 'react';
 import {FormControl, TextField, Input} from '@mui/material';
 import { Button, Box } from '@mui/material';
 export default function Form(props){
-    const [userName,setUserName] = useState('')
-    const [bckClr,setBckClr] = useState('')
-    const [txtClr,setTxtClr] = useState('')
-
-
+    const [formData, setFormData] = useState({
+        userName:'',
+        bckClr:'',
+        txtClr:'',
+    })
 
     function handleSubmit(e){
         e.preventDefault();
-        
-        props.changeData(e.target.username.value);
+        props.changeData(formData);
+    
     }
 
     function handleChange(e){
         e.preventDefault();
-        setUserName(e.target.value)
+        console.log(e.target.name)
+        if(e.target.name ==='username')
+            setFormData(prev=>({...prev, userName: e.target.value}))
+        else if(e.target.name ==='bckclr')
+            setFormData(prev=>({...prev, bckClr: e.target.value}))
+        else if(e.target.name==='txtclr')
+            setFormData(prev=>({...prev,txtClr:e.target.value}))
     }
 
     return(
-    <form style={{textAlign: 'center'}}  onSubmit={handleSubmit}>
+<form style={{textAlign: 'center'}}   onSubmit={handleSubmit}>
 <Input 
   placeholder="Username"
-  value={userName}
+  value={formData.userName}
   name='username'
-  onChange={handleChange}
-/><br/>
-<Input 
-  placeholder="Background Color"
-  value={bckClr}
-  name='bckclr'
-  onChange={handleChange}
-/><br/>
-<Input 
-  placeholder="Text Color"
-  value={txtClr}
-  name='txtclr'
   onChange={handleChange}
 />
 <br/>
-<Button type='submit'>
+<Input 
+variant='contained'
+  placeholder="Background Color"
+  value={formData.bckClr}
+  name='bckclr'
+  onChange={handleChange}
+/>
+<br/>
+<Input 
+  placeholder="Text Color"
+  value={formData.txtClr}
+  name='txtclr'
+  onChange={handleChange}
+/><br/><br/>
+<Button sx={{color:'black',borderColor:'black'}} variant='outlined' type='submit'>
 Change
 </Button>
+
+
 </form>
     )
 }
