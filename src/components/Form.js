9857dfaf1afb596ea2862defee52e73d4fc7bc00
Andrew Alagna <aalagna04@gmail.com>
2022-04-02 12:36:50 -0400

    refactor: make entire form disappear instead of disabling button
    after submission

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,58 +1,64 @@
-import { useState, useEffect } from 'react';
-import {FormControl, TextField, Input} from '@mui/material';
-import { Button, Box } from '@mui/material';
-export default function Form(props){
-    const [formData, setFormData] = useState({
-        userName:'',
-        bckClr:'',
-        txtClr:'',
-    })
-    const [isDisabled,setIsDisabled] = useState(false)
+import { useState, useEffect } from "react";
+import { FormControl, TextField, Input } from "@mui/material";
+import { Button, Box } from "@mui/material";
+export default function Form(props) {
+  const [formData, setFormData] = useState({
+    userName: "",
+    bckClr: "",
+    txtClr: "",
+  });
+  //disable button after click
+  const [isDisabled, setIsDisabled] = useState(false);
 
-    function handleSubmit(e){
-        e.preventDefault();
-        props.changeData(formData);
-        setIsDisabled(true)
-    }
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.changeData(formData);
+    setIsDisabled(true);
+  }
 
-    function handleChange(e){
-        e.preventDefault();
-        if(e.target.name ==='username')
-            setFormData(prev=>({...prev, userName: e.target.value}))
-        else if(e.target.name ==='bckclr')
-            setFormData(prev=>({...prev, bckClr: e.target.value}))
-        else if(e.target.name==='txtclr')
-            setFormData(prev=>({...prev,txtClr:e.target.value}))
-    }
+  function handleChange(e) {
+    e.preventDefault();
+    if (e.target.name === "username")
+      setFormData((prev) => ({ ...prev, userName: e.target.value }));
+    else if (e.target.name === "bckclr")
+      setFormData((prev) => ({ ...prev, bckClr: e.target.value }));
+    else if (e.target.name === "txtclr")
+      setFormData((prev) => ({ ...prev, txtClr: e.target.value }));
+  }
 
-    return(
-<form style={{textAlign: 'center'}}   onSubmit={handleSubmit}>
-<Input 
-  placeholder="Username"
-  value={formData.userName}
-  name='username'
-  onChange={handleChange}
-/>
-<br/>
-<Input 
-variant='contained'
-  placeholder="Background Color"
-  value={formData.bckClr}
-  name='bckclr'
-  onChange={handleChange}
-/>
-<br/>
-<Input 
-  placeholder="Text Color"
-  value={formData.txtClr}
-  name='txtclr'
-  onChange={handleChange}
-/><br/><br/>
-<Button sx={{color:'black',borderColor:'black'}} type='submit' variant='outlined' disabled={isDisabled} >
-Change
-</Button>
-
-
-</form>
-    )
+  return (
+    <form style={{ textAlign: "center" }} onSubmit={handleSubmit}>
+      <Input
+        placeholder="Username"
+        value={formData.userName}
+        name="username"
+        onChange={handleChange}
+      />
+      <br />
+      <Input
+        variant="contained"
+        placeholder="Background Color"
+        value={formData.bckClr}
+        name="bckclr"
+        onChange={handleChange}
+      />
+      <br />
+      <Input
+        placeholder="Text Color"
+        value={formData.txtClr}
+        name="txtclr"
+        onChange={handleChange}
+      />
+      <br />
+      <br />
+      <Button
+        sx={{ color: "black", borderColor: "black" }}
+        type="submit"
+        variant="outlined"
+        disabled={isDisabled}
+      >
+        Change
+      </Button>
+    </form>
+  );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,92 +1,114 @@
-import Clock from './Clock';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Button, Box } from '@mui/material';
-import Form from './Form'
+import Clock from "./Clock";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Button, Box } from "@mui/material";
+import Form from "./Form";
 
-export default function Profile(){
-    const [form,setForm] = useState({
-        user: 'User',
-        bckClr: '',
-        txtClr: ''
-    })
-    const [finData, setFinData] = useState({
-        debit:[],
-        credit:[]
-    })
-    const [debitOrCredit, setDebitOrCredit] = useState('debit')
-    
-    async function getDebits(){
-        const debits = await axios.get('https://moj-api.herokuapp.com/debits')
-        setFinData(prev=>({...prev, debit: debits.data})) 
-    }
+export default function Profile() {
+  const [form, setForm] = useState({
+    user: "User",
+    bckClr: "",
+    txtClr: "",
+  });
+  const [finData, setFinData] = useState({
+    debit: [],
+    credit: [],
+  });
+  const [debitOrCredit, setDebitOrCredit] = useState("debit");
+  const [disableForm, setDisableForm] = useState("false");
 
-    async function getCredits(){
-        const credits = await axios.get('https://moj-api.herokuapp.com/credits')
-        setFinData(prev=>({...prev, credit: credits.data})) 
-    }
+  async function getFinData() {
+    const debits = await axios.get("https://moj-api.herokuapp.com/debits");
+    const credits = await axios.get("https://moj-api.herokuapp.com/credits");
+    setFinData((prev) => ({
+      ...prev,
+      debit: debits.data,
+      credit: credits.data,
+    }));
+  }
 
-    useEffect( () => {
-      getDebits();
-      getCredits();
-      }, []);
+  useEffect(() => {
+    getFinData();
+  }, []);
 
-      const listStyle = {
-        textAlign: 'center', marginBottom:1
-      }
-      const underline = {
-        textDecoration:'underline'
-      }
-      const ulListStyle = {
-        display:'inline-block',justifyContent:'center'
-      }
+  const liListStyle = {
+    textAlign: "center",
+    marginBottom: 1,
+  };
+  const underline = {
+    textDecoration: "underline",
+  };
+  const ulListStyle = {
+    display: "inline-block",
+    justifyContent: "center",
+  };
 
-    function debitList(){
-        const debitlist = finData.debit.map(d =>
-        <li key={d.id}> 
-        <b style={underline}>Description:</b> {d.description}, <b style={underline}> Price:</b> ${d.amount}, <b style={underline}>Date:</b> {d.date.slice(0,10)}
-        </li>
-    ) 
-    return <ul style={ulListStyle}>{debitlist}</ul> 
-}
+  function debitList() {
+    const debitlist = finData.debit.map((d) => (
+      <li key={d.id}>
+        <b style={underline}>Description:</b>
+        {d.description}, <b style={underline}> Price:</b>${d.amount},{" "}
+        <b style={underline}>Date:</b>
+        {d.date.slice(0, 10)}
+      </li>
+    ));
+    return <ul style={ulListStyle}>{debitlist}</ul>;
+  }
 
-     function creditList(){
-         const creditlist = finData.credit.map(c =>
-        <li  key={c.id}> 
-        <b style={underline}>Description:</b> {c.description}, <b style={underline}> Price:</b> ${c.amount}, <b style={underline}>Date:</b> {c.date.slice(0,10)}
-         </li>
-         )
-        return <ul style={ulListStyle}>{creditlist}</ul> 
-    }
+  function creditList() {
+    const creditlist = finData.credit.map((c) => (
+      <li key={c.id}>
+        <b style={underline}>Description:</b> {c.description},{" "}
+        <b style={underline}> Price:</b> ${c.amount},{" "}
+        <b style={underline}>Date:</b> {c.date.slice(0, 10)}
+      </li>
+    ));
+    return <ul style={ulListStyle}>{creditlist}</ul>;
+  }
 
-        const types = ["debit", "credit"];
-        function ToggleData() {
-          const activated = debitOrCredit
-          return (
-            <Box sx={{textAlign: 'center',marginBottom:2}} >
-              {types.map((type) => (
-                <Button sx={{marginRight:1}} variant="contained" active={debitOrCredit === type} onClick={() => setDebitOrCredit(type)}>
-                  {type}
-                </Button>
-              ))}
-            </Box>
-          );
-        }
-        
-        function handleSubmit(formData){
-            setForm(prev=>({...prev, bckClr: formData.bckClr, user: formData.userName, txtClr: formData.txtClr}))
-        }
+  const types = ["debit", "credit"];
+  function ToggleData() {
+    const activated = debitOrCredit;
+    return (
+      <Box sx={{ textAlign: "center", marginBottom: 2 }}>
+        {types.map((type) => (
+          <Button
+            sx={{ marginRight: 1 }}
+            variant="contained"
+            active={debitOrCredit === type}
+            onClick={() => setDebitOrCredit(type)}
+          >
+            {type}
+          </Button>
+        ))}
+      </Box>
+    );
+  }
 
-    return(
-    <Box sx={{backgroundColor: form.bckClr, height: '100vh', color:form.txtClr}} >
-        <Box sx={{display:'flex',  justifyContent: 'center', }}>
-            <h2 style={{marginRight:'10px'}}> Hello {form.user}! </h2> 
-            <Clock/>
-        </Box>
-        <ToggleData />
-      <Box sx={listStyle}>{debitOrCredit ==='debit' ? debitList(): creditList()}<br/> </Box>
-    <Form changeData={handleSubmit}/>
+  function handleSubmit(formData) {
+    setDisableForm("true");
+    setForm((prev) => ({
+      ...prev,
+      bckClr: formData.bckClr,
+      user: formData.userName,
+      txtClr: formData.txtClr,
+    }));
+  }
+
+  return (
+    <Box
+      sx={{ backgroundColor: form.bckClr, height: "100vh", color: form.txtClr }}
+    >
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <h2 style={{ marginRight: "10px" }}> Hello {form.user}! </h2>
+        <Clock />
+      </Box>
+      <ToggleData />
+      <Box sx={liListStyle}>
+        {debitOrCredit === "debit" ? debitList() : creditList()}
+        <br />{" "}
+      </Box>
+      {disableForm === "false" ? <Form changeData={handleSubmit} /> : null}
     </Box>
-    )
+  );
 }
-
